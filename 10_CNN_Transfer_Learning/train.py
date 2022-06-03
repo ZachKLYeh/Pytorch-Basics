d151b701f194cb50f4256d6c288bc408e5232d28
ZachKLYeh <zach.kl.yeh@gmail.com>
2022-06-03 14:08:30 +0800

    modify transfer learning

--- 10_CNN_Transfer_Learning/train.py
+++ 10_CNN_Transfer_Learning/train.py
@@ -19,7 +19,7 @@ learning_rate = 0.001
 #Make dataset with transform included
 #mean = [0.485, 0.456, 0.406]
 #std = [0.229, 0.224, 0.225]
-mean = (0.5, 0.5, 0.5 )
+mean = (0.5, 0.5, 0.5)
 std = (0.5, 0.5, 0.5)
 
 composed_transform = transforms.Compose([transforms.ToTensor(),
